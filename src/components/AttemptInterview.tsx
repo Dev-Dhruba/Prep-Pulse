259800e9e963f59437238248e05145e17a9fb8de
Shreya <shreyashaw1705@gmail.com>
2025-05-20 00:50:16 +0530

    not tested

--- src/components/AttemptInterview.tsx
+++ src/components/AttemptInterview.tsx
@@ -1,17 +1,20 @@
+"use client";
 import React from 'react'
 import Agent from '@/components/Agent';
 import { getInterviewById } from '@/utils/actions';
+import { useAuth } from '@/utils/contexts/UserProvider';
 interface AttemptInterviewProps {
   interviewId: string;
 }
 
 const AttemptInterview = async ({ interviewId }: AttemptInterviewProps) => {
+  const { user } = useAuth();
      const interview = await getInterviewById(interviewId);
      console.log(interview.questions)
   return (
     <Agent
-        userName={"Dhruba Bhattacharyya"}
-        userId={"175c49be-e08e-4716-850e-fc698a47524c"}
+        userName={user?.full_name || ""}
+        userId={user?.id}
         interviewId={interviewId}
         type="interview"
         questions={interview.questions}
